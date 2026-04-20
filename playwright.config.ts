881c297652e6ad5c1b1535cd94a8877b3a7bc3a1
ivanamax23@gmail.com <ivanamax23@gmail.com>
2026-04-20 15:24:24 +0530

    correct code

--- playwright.config.ts
+++ playwright.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
   ],
   use: {
     browserName: 'chromium',
-    headless: true,
+    headless: false,
     screenshot: 'only-on-failure',
     video: 'retain-on-failure',
     trace: 'on-first-retry',
